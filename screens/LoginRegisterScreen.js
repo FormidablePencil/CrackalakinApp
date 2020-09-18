e17bc1ecfa37479c2bc2a35ea1e99ae52d7fb180
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2020-09-18 05:53:30 -0700

    gave a color to welcome screen

--- screens/LoginRegisterScreen.js
+++ screens/LoginRegisterScreen.js
@@ -13,58 +13,63 @@ import LoadingComp from "../components/LoadingComp";
 import { authentication } from '../App'
 import styled from "styled-components";
 import { Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const LoginRegisterScreen = ({ navigation }) => {
   // const { setLoginScreenReady } = useContext(MatchingGameContext)
   const [loginScreenReady, setLoginScreenReady] = useState(false)
 
   return (
-    <View style={{ position: 'relative', height: '100%' }}>
-      <Video
-        onReadyForDisplay={() => {
-          setLoginScreenReady(true)
-          LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
-        }}
-        source={vid}
-        rate={1.0}
-        volume={0}
-        isMuted={false}
-        resizeMode="cover"
-        shouldPlay
-        isLooping
-        style={{ width: '100%', height: '100%', position: 'absolute' }}
-      />
+    <LinearGradient colors={['#FFB755', '#FF645B']}>
+      <View style={{ position: 'relative', height: '100%', background: 'transparent' }}>
+        <Video
+          onReadyForDisplay={() => {
+            setLoginScreenReady(true)
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+          }}
+          source={vid}
+          rate={1.0}
+          volume={0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={{ width: '100%', height: '100%', position: 'absolute' }}
+        />
 
-      {!authentication ?
-        <CenterView>
-          <H1
-            style={{ position: 'absolute', top: 110 }}
-          >What's</H1>
-          <H1
-            style={{ position: 'absolute', top: 150 }}
-          >Crackilackin</H1>
-          <PlayBtn
-            onPress={() => {
-              setLoginScreenReady(false)
-              navigation.navigate("Game")
-            }}
-          ><H1>Play</H1></PlayBtn>
-        </CenterView>
-        :
-        <>
-          <Bg>
-            <H1>Crackilackin</H1>
-            <KeyboardAvoidingView style={{ width: "100%" }} behavior="position">
-              <EnteringOptionsComp navigation={navigation} setLoginScreenReady={setLoginScreenReady} />
-            </KeyboardAvoidingView>
-          </Bg>
-          {loginScreenReady === false &&
-            <LoadingComp />
-          }
-        </>
-      }
+        {!authentication ?
+          <CenterView>
+            <H1
+              style={{ position: 'absolute', top: 110 }}
+            >What's</H1>
+            <H1
+              style={{ position: 'absolute', top: 150 }}
+            >Crackilackin</H1>
+            <View style={{ marginTop: 200 }}>
+              <PlayBtn
+                onPress={() => {
+                  setLoginScreenReady(false)
+                  navigation.navigate("Game")
+                }}
+              ><H1>Play</H1></PlayBtn>
+            </View>
+          </CenterView>
+          :
+          <>
+            <Bg>
+              <H1>Crackilackin</H1>
+              <KeyboardAvoidingView style={{ width: "100%" }} behavior="position">
+                <EnteringOptionsComp navigation={navigation} setLoginScreenReady={setLoginScreenReady} />
+              </KeyboardAvoidingView>
+            </Bg>
+            {loginScreenReady === false &&
+              <LoadingComp />
+            }
+          </>
+        }
 
-    </View>
+      </View>
+    </LinearGradient>
   );
 };
 
